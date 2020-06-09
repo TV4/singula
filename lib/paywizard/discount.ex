4a233975d6ed4baf2520bf7596106d8455205fd9
robinch <robinch88@gmail.com>
2020-06-09 11:26:30 +0200

    Make campagin and source types binary and nil

--- lib/paywizard/discount.ex
+++ lib/paywizard/discount.ex
@@ -1,5 +1,5 @@
 defmodule Paywizard.Discount do
   defstruct [:discount, is_single_use: false, promotion: "NONE", campaign: "NONE", source: "NONE"]
 
-  @type t :: %__MODULE__{discount: binary | nil, promotion: binary, campaign: binary, source: binary}
+  @type t :: %__MODULE__{discount: binary | nil, promotion: binary, campaign: binary | nil, source: binary | nil}
 end
